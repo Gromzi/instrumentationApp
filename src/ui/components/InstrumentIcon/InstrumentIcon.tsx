f702d6d2ceea6727b2f807757917be560afa190f
Konrad Chęciński <Konradchecinski2012@gmail.com>
2025-06-09 23:43:41 +0200

    use midiplayer in MusicPlayer

--- src/ui/components/InstrumentIcon/InstrumentIcon.tsx
+++ src/ui/components/InstrumentIcon/InstrumentIcon.tsx
@@ -3,19 +3,19 @@ import "./InstrumentIconStyles.css"
 import threeDotsIcon from "../../assets/three-dots.svg"
 
 const InstrumentIcon = (props: InstrumentIconProps) => {
-  return (
-    <div className="instrument-icon-container">
-      <div className="three-dots-container">
-        <img src={threeDotsIcon} alt="Settings" className="three-dots icon" />
-      </div>
+    return (
+        <div className="instrument-icon-container">
+            <div className="three-dots-container">
+                <img src={threeDotsIcon} alt="Settings" className="three-dots icon" />
+            </div>
 
-      <img
-        src={props.icon}
-        className={props.active ? "icon pulse glow" : "icon"}
-        alt="Instrument logo"
-      />
-    </div>
-  )
+            <img
+                src={props.icon}
+                className={props.active ? "icon pulse glow" : "icon"}
+                alt="Instrument logo"
+            />
+        </div>
+    )
 }
 
 export default InstrumentIcon
--- src/ui/components/MusicPlayer/MusicPlayer.tsx
+++ src/ui/components/MusicPlayer/MusicPlayer.tsx
@@ -2,32 +2,46 @@ import RateIcon from "../RateButton/RateButton"
 import "./MusicPlayerStyles.css"
 import playIcon from "../../assets/play.svg"
 import { InstrumentIcons } from "../../constants/InstrumentIconsImports"
+import {useMidiPlayer} from "../../hooks/useMidiPlayer.tsx";
 
 const MusicPlayer = () => {
+    const {handleStart, handlePause, handleNextInstrument,instrument,songTitle, isPlaying, duration, currentTime, formatTime} = useMidiPlayer()
+
+
+    const handlePlayPause = () => {
+        if(isPlaying){
+            handlePause()
+        }
+        else {
+            handleStart()
+        }
+    }
+
+
   return (
     <div className="music-player-container">
       <div className="instrument-info">
         <div className="current-instrument" id="currentInstrument">
-          Guitar
+            {instrument?.label}
         </div>
-        <div className="song-title">Song Title</div>
+        <div className="song-title">{songTitle}</div>
       </div>
 
       <div className="progress-section">
         <div className="time-display">
-          <span id="currentTime">00:15</span>
-          <span id="totalTime">03:00</span>
+          <span id="currentTime">{formatTime(currentTime)}</span>
+          <span id="totalTime">{formatTime(duration)}</span>
         </div>
 
         <div className="progress-track">
-          <div className="progress-fill" id="progressFill"></div>
+          <div className="progress-fill" id="progressFill" style={{width: (currentTime/duration)*100+"%"}}></div>
         </div>
       </div>
 
       <div className="controls">
         <RateIcon type="like" />
 
-        <button className="control-button play-pause-btn" id="playPauseBtn">
+        <button className="control-button play-pause-btn" id="playPauseBtn" onClick={handlePlayPause}>
           <img src={playIcon} alt="Play icon" />
         </button>
 
@@ -35,6 +49,7 @@ const MusicPlayer = () => {
           className="control-button next-instrument-btn violin"
           id="nextInstrumentBtn"
           title="Next Instrument"
+          onClick={()=> handleNextInstrument()}
         >
           <img src={InstrumentIcons.violin} alt="Next instrument icon" />
         </button>
