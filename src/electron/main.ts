97adf819cb5189e65282d1e3bfd989161176c468
Konrad Chęciński <Konradchecinski2012@gmail.com>
2025-06-09 23:40:31 +0200

    add preload to electron

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,20 +2,30 @@ import { app, BrowserWindow } from "electron"
 import path from "path"
 import { isDev } from "./util.js"
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({
-    width: 580,
-    minWidth: 580,
+app.whenReady().then(() => {
+    const mainWindow = new BrowserWindow({
+        width: 580,
+        minWidth: 580,
 
-    height: 640,
-    minHeight: 640,
+        height: 640,
+        minHeight: 640,
 
-    center: true,
-  })
+        center: true,
 
-  if (isDev()) {
-    mainWindow.loadURL("http://localhost:5123")
-  } else {
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
-  }
+        title: "Wybieranie instrumentacji",
+
+        webPreferences: {
+            preload: path.join(app.getAppPath(), "/dist-electron/preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true
+
+        }
+    })
+
+    if (isDev()) {
+        mainWindow.webContents.openDevTools();
+        mainWindow.loadURL("http://localhost:5123")
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
+    }
 })
---
+++ src/electron/preload.ts
@@ -0,0 +1,24 @@
+import { contextBridge } from 'electron'
+
+// Definiujemy interfejs dla naszego API
+interface ElectronAPI {
+    printHello: () => void;
+}
+
+// Eksportujemy nasze API
+const electronAPI: ElectronAPI = {
+    printHello: () => {
+        console.log('Hello from preload');
+    },
+
+}
+
+// Eksponujemy API do window.electron
+contextBridge.exposeInMainWorld('electron', electronAPI);
+
+// Dla TypeScript - deklaracja typów
+declare global {
+    interface Window {
+        electron: ElectronAPI
+    }
+}
